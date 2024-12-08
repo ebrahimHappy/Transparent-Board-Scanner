3b79b43d6da205de4c6d2bc0057972dd399a376a
Abe <>
2024-12-08 21:26:06 +0330

    added initial version of logic

---
+++ transparent_board_scanner/main.py
@@ -0,0 +1,97 @@
+
+# %%
+import cv2 as cv
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# %%
+images = [
+    cv.imread(f'../samples/hello-world/image{i}.jpg')[...,::-1]
+    for i in range(1,7)
+]
+
+
+# %%
+
+
+
+class ImageAligner:
+    def __init__(self):
+        self.sift = cv.SIFT_create()
+
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        self.flann = cv.FlannBasedMatcher(index_params, search_params)
+
+        self.good_match_ratio = 0.7
+
+        self.min_match_count = 10
+
+    def _calculate_keypoints_descriptors(self, image):
+        grayscale = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+        keypoints, descriptors = self.sift.detectAndCompute(grayscale, None)
+        return keypoints, descriptors
+
+    def _filter_good_matches(self, matches):
+        return [m for m, n in matches if m.distance / n.distance < self.good_match_ratio]
+
+    def _find_homography_transform(self, matches, keypoints1, keypoints2):
+        src_pts = np.float32([keypoints1[m.queryIdx].pt for m in matches]).reshape(-1,1,2)
+        dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in matches]).reshape(-1,1,2)
+        transform, _ = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+        return transform
+
+    def set_reference_image(self, image):
+        keypoints, descriptors = self._calculate_keypoints_descriptors(image)
+        self.reference_keypoints = keypoints
+        self.reference_descriptors = descriptors
+        self.reference_shape = image.shape[:2]
+        return self
+
+    def align_image(self, image):
+        keypoints, descriptors = self._calculate_keypoints_descriptors(image)
+        matches = self.flann.knnMatch(descriptors, self.reference_descriptors, k=2)
+        good_matches = self._filter_good_matches(matches)
+        if len(good_matches) < self.min_match_count:
+            return None
+        transform = self._find_homography_transform(good_matches, keypoints, self.reference_keypoints)
+        # matchesMask = mask.ravel().tolist()
+        aligned = cv.warpPerspective(image, transform, self.reference_shape[::-1])
+        return aligned
+
+# %%
+
+aligner = ImageAligner().set_reference_image(images[0])
+aligned = [images[0]] + [aligner.align_image(img) for img in images[1:]]
+
+# %%
+
+aligned_g = [cv.cvtColor(img, cv.COLOR_RGB2GRAY) for img in aligned]
+
+
+
+# %%
+
+def highpass(img, sigma):
+    return img - cv.GaussianBlur(img, (0,0), sigma) + 127
+
+
+# %%
+agg = []
+for img in aligned:
+    hp = highpass(img, 10)
+    hp = np.clip(hp, 0, 127)
+    agg.append(hp)
+result = (np.median(agg, axis=0) * 2).astype('uint8')
+
+plt.imshow(result)
+
+# cv.imwrite('result.jpg', result[...,::-1])
+
+# %%
+# for i in range(3):
+#     cv.imwrite(f'align{i}.jpg', aligned[i][...,::-1])
+
+# %%
